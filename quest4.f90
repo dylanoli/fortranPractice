03f641d694960f44f68bb549b2a35387eed7dce0
unknown <dylan.oli@hotmail.com>
2019-10-20 16:03:14 -0300

    quenstões 4 e 5

---
+++ quest4.f90
@@ -0,0 +1,25 @@
+program quest4
+    implicit none
+
+    real num1,num2
+
+    print*,"Digite num1:"
+    read (*,*),num1
+    print*,"Digite num2:"
+    read (*,*),num2
+
+    print*,"Adicao:"
+    print "(f6.2)",(num1 + num2)
+
+    print*,"Subtracao:"
+    print "(f6.2)",(num1 - num2)
+
+    print*,"Multiplicacao:"
+    print "(f6.2)",(num1*num2)
+
+    print*,"Divisao:"
+    print"(f6.2)",(num1/num2)
+
+    print*,"Resto:"
+    print"(f6.2)",MOD(num1,num2)
+end program
---
+++ quest6.f90
@@ -0,0 +1,7 @@
+program quest6
+    implicit none
+
+    real num1/10/,num2/7/,num3/8/
+
+    print*,"Media:",(num1+num2+num3)/3
+end program
